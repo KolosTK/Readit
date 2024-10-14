c2f06d73c694146809e8d05c95bb023596932cb7
KolosTK <tanya.kolosenko.1705@gmail.com>
2024-10-14 12:22:03 +0300

    Add data access library

--- Readit.DataAccess/Data/ApplicationDbContext.cs
+++ Readit.DataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+namespace Readit.DataAccess.Data;
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
--- Readit/Program.cs
+++ Readit/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Readit.DataAccess.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
