1d3dec3399e454e200730d3176e87852d3043519
KolosTK <tanya.kolosenko.1705@gmail.com>
2024-10-14 11:41:54 +0300

    Add models library

--- Readit/Data/ApplicationDbContext.cs
+++ Readit/Data/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Readit.Data;
-
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
--- Readit/Program.cs
+++ Readit/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Readit.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
