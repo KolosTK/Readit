126aec98cde1dd84acbc9b648692cdf535a3c0fc
KolosTK <tanya.kolosenko.1705@gmail.com>
2024-10-16 13:45:32 +0300

    Add data annotation to Book and Category models

--- Readit.Models/Book.cs
+++ Readit.Models/Book.cs
@@ -1,17 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Readit.Models;
 
 public class Book
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
     public string Title { get; set; }
+    
+    [Required]
     public string ISBN { get; set; }
+    
+    [Required]
     public string Author { get; set; }
-    public List<int> GlobalRate { get; set; }
-    public int Rate { get; set; }
-    public Category Category { get; set; }
+    
+    public int? GlobalRate { get; set; }
+    public int? Rate { get; set; }
+    
+    public int? CategoryId { get; set; }
+    public Category? Category { get; set; }
+    
     public string Cover { get; set; }
     public string Summary { get; set; }
     public int PagesNumber { get; set; }
+    
+    [MaxLength(2024)]
     public int YearOfPublishing { get; set; }
-    public IEnumerator<Comment> Comments { get; set; }
 }
