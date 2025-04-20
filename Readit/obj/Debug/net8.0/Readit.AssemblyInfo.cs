6d2c66919b2b8899881bc62e1e575f5dd5d3d473
KolosTK <tanya.kolosenko.1705@gmail.com>
2025-04-20 17:21:36 +0300

    commit before merging with main

--- Readit/obj/Debug/net8.0/Readit.AssemblyInfo.cs
+++ Readit/obj/Debug/net8.0/Readit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Readit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d9740ae30ebf1944e4e67c6bc3cfa379ee7ccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945a1a5a866275de2e3ae841ad5be2b4ec0be200")]
 [assembly: System.Reflection.AssemblyProductAttribute("Readit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Readit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
