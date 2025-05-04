9c78d6b0c240cc729574eaf2c78eae456dcd46b4
KolosTK <tanya.kolosenko.1705@gmail.com>
2025-05-04 20:40:21 +0300

    Add enum ReadingSTatus

---
+++ Readit.Models/ReadingStatus.cs
@@ -0,0 +1,11 @@
+namespace Readit.Models;
+
+public enum ReadingStatus
+{
+    PlanToRead,
+    Reading,
+    Finished,
+    OnHold,
+    Dropped,
+    Rereading
+}
--- Readit/obj/Debug/net8.0/Readit.AssemblyInfo.cs
+++ Readit/obj/Debug/net8.0/Readit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Readit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaac074b213ad6b36040ca8447fa2e641ade5bc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af58e8f795015c896859034c284fa9a7b31deda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Readit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Readit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
