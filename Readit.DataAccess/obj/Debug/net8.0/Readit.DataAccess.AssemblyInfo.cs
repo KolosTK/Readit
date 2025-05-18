0c67ccd77fa1dff0038034b6ba8badc3bd7e8d9a
KolosTK <tanya.kolosenko.1705@gmail.com>
2025-05-18 18:17:56 +0300

    Avatars changers

--- Readit.DataAccess/obj/Debug/net8.0/Readit.DataAccess.AssemblyInfo.cs
+++ Readit.DataAccess/obj/Debug/net8.0/Readit.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Readit.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b5cdb99f28c64b3ab080823f775a39169480006")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b27b587f99ada71946b920e87057d27983d370a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Readit.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Readit.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
