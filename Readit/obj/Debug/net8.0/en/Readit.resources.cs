310d4f6d3cda91e1a2796ff55aa3f6221d2d97d5
KolosTK <tanya.kolosenko.1705@gmail.com>
2025-05-27 13:48:19 +0300

    Add correctives to view

--- Readit/obj/Debug/net8.0/en/Readit.resources.cs
+++ Readit/obj/Debug/net8.0/en/Readit.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Readit")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd3b7fb48af085621122c25ecf2c653583c8fd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86bcafb8570e2619d9972f053185a7f636267d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Readit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Readit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
