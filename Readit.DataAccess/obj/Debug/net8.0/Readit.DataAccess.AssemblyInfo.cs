e376ce743e81b3d18d7610104fcd5793e15f77ea
KolosTK <tanya.kolosenko.1705@gmail.com>
2025-07-16 12:22:32 +0300

    Fix conflict

--- Readit.DataAccess/obj/Debug/net8.0/Readit.DataAccess.AssemblyInfo.cs
+++ Readit.DataAccess/obj/Debug/net8.0/Readit.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Readit.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c318058df507be49d99e381eff3adf093530118")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782b5caac056e289eab06714d12c44e753afbf7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Readit.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Readit.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
