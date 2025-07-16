3c8487f1b581a9fdf2ebbf7c8c945a597c605a34
KolosTK <tanya.kolosenko.1705@gmail.com>
2025-07-16 11:51:14 +0300

    Add testing functionality

--- Readit/obj/Debug/net8.0/en/Readit.resources.cs
+++ Readit/obj/Debug/net8.0/en/Readit.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Readit")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1476127d6870c3abe33dd5a5d42c1bd63c5a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f3668d4545d8d21def48b65a7e6bdfc57f53df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Readit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Readit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
